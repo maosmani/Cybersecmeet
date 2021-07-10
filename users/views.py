4014dc856853a9a8cd1598e2b24ec1ca44785d3a
Mohamed Amine Osmani <mohamed.amine.osmani.job@gmail.com>
2021-07-10 23:25:17 +0200

    first commit

---
+++ users/views.py
@@ -0,0 +1,39 @@
+
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def index(request):
+
+	return HttpResponse("hello world!")
+# Create your views here.
+
+def login_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        ...
+    else:
+        # Return an 'invalid login' error message.
+        ...
