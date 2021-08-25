bf4df06d25e00ac66d5410924a0e0abde9d7f3f1
Mohamed Amine Osmani <mohamed.amine.osmani.job@gmail.com>
2021-08-25 08:50:48 +0200

    hello world

--- meetings/models.py
+++ meetings/models.py
@@ -10,7 +10,7 @@ class Meetings(models.Model):
     field = models.CharField(max_length=100, choices=MY_Field,default = 'choose your field')
     topic = models.CharField(max_length=250)
     title = models.CharField(max_length=250)
-    about_meeting = models.TextField()
+    about_meeting = models.TextField(max_length=500)
     zoom_url = models.URLField(max_length=200)
     time =  models.TimeField(null=True)
     date = models.DateField(null=True)
