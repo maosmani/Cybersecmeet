f326d6d69c2991a2267bd84556382e0fa541cd1e
Mohamed Amine Osmani <mohamed.amine.osmani.job@gmail.com>
2021-10-07 20:26:20 +0200

    hello world

--- meetings/serializers.py
+++ meetings/serializers.py
@@ -12,4 +12,4 @@ class MeetingSerializer(serializers.ModelSerializer):
 class UsersSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = NewUser
-		fields = ('id','who_is','admin_key')
+		fields = ('id','email','user_name','who_is','admin_key')
